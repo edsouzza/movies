5b42167f6a4a864534f34a0d2f0db5075e0a51cd
Edi Aquino de Souza <edsouzza@hotmail.com>
2023-11-18 10:42:14 -0300

    created header and footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import styles from './Footer.module.css'
+
+function Footer(){
+    return(
+        <footer className={styles.footer}>
+            <h2>EdsoFlix &copy; Desenvolvido por Edsof 2023</h2>
+        </footer>
+    )
+}
+export default Footer;
