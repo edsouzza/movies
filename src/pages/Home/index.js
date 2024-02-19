5fbb8a184c5a91e82420d25a4b58300018794742
Edi Aquino de Souza <edsouzza@hotmail.com>
2024-02-19 03:21:22 -0300

    feat:update

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,11 +6,12 @@ import Header from "../../components/Header";
 import Card from '../../components/Card';
 
 function Home() {
+ 
   return (
     <>
       <Header />
       <Banner image="favoritos"/>
-      <Container>      
+      <Container>  
           { categories.map((category, index) =>
             <Category category={category}>
             { filterCategory(index).map((video) => <Card id={video.id} key={video.id} />) }
